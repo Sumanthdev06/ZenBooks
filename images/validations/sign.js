511c614de03ffc25c86da90bbb6b50c58a364fb0
Sumanth Reddy <101918709+Sumanthdev06@users.noreply.github.com>
2023-06-24 18:16:44 +0530

    Update sign.js

--- images/validations/sign.js
+++ images/validations/sign.js
@@ -112,7 +112,7 @@ function signInWithGmail() {
   // Redirect the user to the Gmail authorization page
   var clientId =
     "805990898205-e9ajof1kiie13dvsmq27u6djdrnv6lmr.apps.googleusercontent.com";
-  var redirectUri = "https://sumanthdev06.github.io/User/"; // Replace with your actual redirect URI
+  var redirectUri = "https://sumanthdev06.github.io/User"; // Replace with your actual redirect URI
   var scope = "email profile";
   var state = "YOUR_STATE"; // Replace with your actual state value
 
